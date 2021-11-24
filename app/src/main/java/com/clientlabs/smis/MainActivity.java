fe87e04aa9549fbc2554ed2d1ece30a50e47dbfc
momutuku <devophx@gmail.com>
2021-11-24 21:51:50 +0300

    Started on Dashboard

--- app/src/main/java/com/clientlabs/smis/MainActivity.java
+++ app/src/main/java/com/clientlabs/smis/MainActivity.java
@@ -1,13 +1,53 @@
 package com.clientlabs.smis;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    BottomNavigationView bottomNavigationView;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigationMyCourses:
+                case R.id.navigationHome:
+                    return true;
+                case R.id.navigationSearch:
+                    return true;
+//                case R.id.navigationMenu:
+//                    DrawerLayout drawer = findViewById(R.id.drawer_layout);
+//                    drawer.openDrawer(GravityCompat.START);
+//                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        bottomNavigationView.setSelectedItemId(R.id.navigationHome);
+
+
     }
 }
