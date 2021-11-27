64f785c002f271a837cd70d1e517d50680fdae34
momutuku <devophx@gmail.com>
2021-11-27 11:46:00 +0300

    Dashboard: Added drawer

--- app/src/main/java/com/clientlabs/smis/MainActivity.java
+++ app/src/main/java/com/clientlabs/smis/MainActivity.java
@@ -1,17 +1,32 @@
 package com.clientlabs.smis;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toolbar;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
+import com.clientlabs.smis.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.appcheck.FirebaseAppCheck;
 import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory;
@@ -21,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     BottomNavigationView bottomNavigationView;
+    DrawerLayout drawer;
+    NavigationView navigationView;
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+    ImageView hum, notification;
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -43,11 +64,33 @@ public class MainActivity extends AppCompatActivity {
     };
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.tool, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_notification:
+                Toast.makeText(MainActivity.this, "Notification", Toast.LENGTH_SHORT).show();
+                return true;
+            case android.R.id.home:
+                drawer.openDrawer(Gravity.LEFT);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        // TODO: 11/27/2021 Enable below part when going into production, disable emulator bit 
+        // TODO: 11/27/2021 Enable below part when going into production, disable emulator bit
 //        FirebaseApp.initializeApp(/*context=*/ this);
 //        FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
 //        firebaseAppCheck.installAppCheckProviderFactory(SafetyNetAppCheckProviderFactory.getInstance());
@@ -59,11 +102,51 @@ public class MainActivity extends AppCompatActivity {
                 DebugAppCheckProviderFactory.getInstance());
 //        End Emulator bit
 
+
         toolbar = findViewById(R.id.toolbar);
+
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+
         bottomNavigationView.setSelectedItemId(R.id.navigationHome);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.appBarHome.toolbar);
+
+        drawer = binding.drawer;
+        navigationView = binding.navView;
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.hum);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.setDrawerIndicatorEnabled(true);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home)
+                .setDrawerLayout(drawer)
+                .build();
+
+        toolbar.inflateMenu(R.menu.tool);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.nav_fees) {
+
+//                    Intent fees_int = new Intent(MainActivity.this, Fees.class);
+//                    login.putExtra("role", "DriverInfo");
+//                    startActivity(fees_int);
+//                    Animatoo.animateSlideUp(Fees.this);
+//                    drawer.closeDrawer(Gravity.RIGHT);
+                }
+                return false;
+            }
+        });
 
 
     }
